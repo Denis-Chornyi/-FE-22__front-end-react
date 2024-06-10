be22bff0429046f6ce0ad750d4ef1b7225edb203
Denis Chornyi <chornyi.den@gmail.com>
2024-06-10 20:30:08 +0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const rootElement = document.querySelector('#root');
 
-const greetingElem = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
+const element = React.createElement('div', { className: 'greeting' }, [
+  React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
+  React.createElement('div', { className: 'greeting__text' }, "I'm learning React")
+]);
 
-ReactDOM.render(greetingElem, rootElement);
+ReactDOM.render(element, rootElement);
