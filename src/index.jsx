284650185968fcd5066f2c5bfddad4cf6f4b78bf
Denis Chornyi <chornyi.den@gmail.com>
2024-06-11 16:32:45 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+export const renderSecons = time => {
+  const seconds = new Date(time).getSeconds();
+
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+
+  const styles = {
+    color: textColor,
+    backgroundColor
+  };
+
+  const element = (
+    <div className="seconds" style={styles}>
+      {seconds}
+    </div>
+  );
+
+  ReactDOM.render(element, rootElement);
+};
+
+renderSecons(new Date());
+
+setInterval(() => renderSecons(new Date()), 1000);
