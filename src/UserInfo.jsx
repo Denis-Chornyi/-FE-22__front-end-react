9590f96539e739ea482b67cb05e36fa26d9982c1
Denis Chornyi <chornyi.den@gmail.com>
2024-06-12 14:12:56 +0300

    fix

--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Avatar from './Avatar';
+import './userInfo.scss';
 
 const UserInfo = props => {
   return (
     <div className="user-info">
-      <Avatar 
-      avatarUrl={props.user.avatarUrl}
-      name={props.user.name} 
-      />
+      <Avatar avatarUrl={props.user.avatarUrl} name={props.user.name} />
       <div className="user-info__name">{props.user.name}</div>
     </div>
   );
