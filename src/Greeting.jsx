c98e44f5958bf5894b20b9ed066da178e62dede7
Denis Chornyi <chornyi.den@gmail.com>
2024-06-12 15:16:08 +0300

    fix

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Greeting = props => {
   return (
-    <div className="greeting">{`My name is ${props.user.firstName} ${props.user.lastName}. I am ${props.user.age()} years old`}</div>
+    <div className="greeting">{`My name is ${props.user.firstName} ${
+      props.user.lastName
+    }. I am ${Math.floor(
+      (new Date() - props.user.birthDate) / (1000 * 60 * 60 * 24 * 30 * 12)
+    )} years old`}</div>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,6 @@ const userInfo = {
   firstName: 'John',
   lastName: 'Doe',
   birthDate: new Date(2007, 0, 1),
-  age() {
-    const userAge = new Date() - this.birthDate;
-    return Math.floor(userAge / (1000 * 60 * 60 * 24 * 30 * 12));
-  }
 };
 
 ReactDOM.render(<Greeting user={userInfo} />, document.querySelector('#root'));
