3a1162f3da7f962c99eb2706109290c1e38399d4
Denis Chornyi <chornyi.den@gmail.com>
2024-06-12 16:36:14 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Greeting from './Greeting';
+import Profile from './Profile';
 
-ReactDOM.render(<Greeting firstName="Adam" lastName="Sandler" birthDate={new Date('2001-01-01T11:11:11.819Z')} />, document.querySelector('#root'));
+const userData = {
+  firstName: 'James',
+  lastName: 'Bond',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London'
+};
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<Profile user={userData} />, root);
