8065f9ce7d3784a56aaf8e7497c00c92aaf099c0
Denis Chornyi <chornyi.den@gmail.com>
2024-06-13 18:00:09 +0300

    fix

--- src/GoodButton.jsx
+++ src/GoodButton.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const GoodButton = () => (
-  <button className="fancy-button" onClick={() => alert('Good job!')}>
-    Click me!
-  </button>
-);
+class GoodButton extends React.Component {
+  handelClick(e) {
+    alert(e.target.textContent);
+  }
+  render() {
+    return (
+      <button className="fancy-button" onClick={this.handelClick}>
+        Click me!
+      </button>
+    );
+  }
+}
 
 export default GoodButton;
