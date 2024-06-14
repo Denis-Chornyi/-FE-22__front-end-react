997c310fa3f81ebf5700488742f1bea6ed9cf7d0
Denis Chornyi <chornyi.den@gmail.com>
2024-06-14 20:26:25 +0300

    fix

--- src/Page.jsx
+++ src/Page.jsx
@@ -17,16 +17,20 @@ class Page extends React.Component {
   render() {
     return (
       <div className="page">
-        <Info text={this.state.text} />
-        <button className="btn" onClick={() => this.setText(text1)}>
-          IPhone 13
-        </button>
-        <button className="btn" onClick={() => this.setText(text2)}>
-          IPhone 13 Pro
-        </button>
-        <button className="btn" onClick={() => this.setText('')}>
-          Clear
-        </button>
+        <div className="text">
+          <Info text={this.state.text} />
+        </div>
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText(text1)}>
+            IPhone 13
+          </button>
+          <button className="btn" onClick={() => this.setText(text2)}>
+            IPhone 13 Pro
+          </button>
+          <button className="btn" onClick={() => this.setText('')}>
+            Clear
+          </button>
+        </div>
       </div>
     );
   }
