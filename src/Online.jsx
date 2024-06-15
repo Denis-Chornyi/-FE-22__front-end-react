be33e4550a709dc00ba7501fa6b8bed8a863f63f
Denis Chornyi <chornyi.den@gmail.com>
2024-06-15 10:56:09 +0300

    fix

--- src/Online.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Online = ({ toggleOffline }) => (
-  <span className="status__text" onClick={toggleOffline}>
-    All good!
-  </span>
-);
-
-export default Online;
--- src/Status.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Online from './Online';
-import Offline from './Offline';
-
-const Status = ({ isOnline, toggleOnline, toggleOffline }) => {
-  return isOnline ? <Online toggleOffline={toggleOffline} /> : <Offline toggleOnline={toggleOnline} />;
-};
-
-export default Status;
