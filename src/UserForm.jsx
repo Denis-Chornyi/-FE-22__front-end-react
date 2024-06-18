7476f2cdc9a1af2d442a976cace7f85fad8f7700
Denis Chornyi <chornyi.den@gmail.com>
2024-06-18 17:10:43 +0300

    fix

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class UserForm extends React.Component {
+class UserForm extends Component {
+  formRef = React.createRef();
+
   handleSubmit = event => {
     event.preventDefault();
-    const formData = [...new FormData(this.formRef)].reduce(
-      (acc, [name, value]) => ({
-        ...acc,
-        [name]: value
-      }),
-      {}
-    );
+    const formData = new FormData(this.formRef.current);
+    const formDataObject = {};
 
-    formData.student === 'on' ? (formData.student = true) : (formData.student = false);
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
+    });
 
-    this.props.onSubmit(formData);
-  };
-
-  setRef = node => {
-    this.formRef = node;
+    this.props.onSubmit(formDataObject);
   };
 
   render() {
     return (
-      <form ref={this.setRef} className="login-form" onSubmit={this.handleSubmit}>
+      <form ref={this.formRef} className="login-form" onSubmit={this.handleSubmit}>
         <h1 className="form-title">Profile</h1>
 
         <div className="form-control">
@@ -40,22 +35,22 @@ class UserForm extends React.Component {
         </div>
 
         <div className="form-control">
-          <label className="form-label" id="occupation" htmlFor="occupation">
+          <label className="form-label" htmlFor="occupation">
             Occupation
           </label>
-          <select name="occupation" className="form-input">
+          <select name="occupation" className="form-input" id="occupation">
             <option value="london">London</option>
             <option value="new-york">New York</option>
-            <option value="coconut">Sidney</option>
-            <option value="mango">Berlin</option>
+            <option value="sidney">Sidney</option>
+            <option value="berlin">Berlin</option>
           </select>
         </div>
 
         <div className="form-control">
-          <label className="form-label" id="about" htmlFor="about">
+          <label className="form-label" htmlFor="about">
             About
           </label>
-          <textarea name="about" className="form-input" />
+          <textarea name="about" className="form-input" id="about" />
         </div>
 
         <button className="submit-button" type="submit">
