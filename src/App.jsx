38d37de77ffdf655fdbd810624735f2e80433ec5
Denis Chornyi <chornyi.den@gmail.com>
2024-06-19 21:48:37 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,8 @@ class App extends React.Component {
       <div className="page">
         <h1 className="title">{`Hello ${userData.firstName} ${userData.lastName}`}</h1>
         <main className="content">
-          <ShoppingCart userData={this.state.userData} />
-          <Profile userData={this.state.userData} handleChange={this.handleChange} />
+          <ShoppingCart userData={userData} />
+          <Profile userData={userData} handleChange={this.handleChange} />
         </main>
       </div>
     );
