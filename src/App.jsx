31600992906991632dc4c3d9b2c1a7f27938832e
Denis Chornyi <chornyi.den@gmail.com>
2024-06-19 14:24:01 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import UsersList from './UsersList';
 
 const App = () => {
   const users = [
-    { id: 1, name: 'John Doe', age: 25 },
-    { id: 2, name: 'Bob Doe', age: 18 },
-    { id: 3, name: 'Sam Smith', age: 23 },
-    { id: 4, name: 'John Smith', age: 54 },
+    { id: 1, name: 'Tad', age: 18 },
+    { id: 2, name: 'Anna', age: 45 }
   ];
 
   return <UsersList users={users} />;
