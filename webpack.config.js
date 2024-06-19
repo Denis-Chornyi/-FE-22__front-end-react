7c47c8f84fc80d6533c174b679c3a9bdb5e00768
Denis Chornyi <chornyi.den@gmail.com>
2024-06-19 16:17:14 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
-const path = require('path');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
   const config = {
     entry: './src/index.jsx',
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'build')
+      filename: 'bundle.js'
     },
     module: {
       rules: [
@@ -43,7 +41,7 @@ module.exports = (env, argv) => {
       historyApiFallback: true,
       open: true,
       hot: true,
-      port: 1080
+      port: 8080
     }
   };
 
