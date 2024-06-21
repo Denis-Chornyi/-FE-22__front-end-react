d2fc787153e4525991da7f1d4f6b07a5661dd041
Denis Chornyi <chornyi.den@gmail.com>
2024-06-21 17:11:34 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import Expand from './Expand';
-import { SlArrowUp, SlArrowDown } from 'react-icons/sl';
 
-class App extends React.Component {
-  state = {
-    isOpen: false
-  };
-  btnExtend = () => {
-    this.setState(() => ({
-      isOpen: this.state.isOpen === false ? true : false
-    }));
-  };
-
-  render() {
-    const { isOpen } = this.state;
-    const icon = isOpen ? <SlArrowUp /> : <SlArrowDown />;
-
-    return (
-      <div className="app">
-        <Expand isOpen={isOpen} icon={icon} title="Some title" onToggle={this.btnExtend}>
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and other React features
-            without writing a class.
-          </p>
-        </Expand>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Expand title="Some title">
+        <p>
+          Hooks are a new addition in React 16.8. They let you use state and other React features
+          without writing a class.
+        </p>
+      </Expand>
+    </div>
+  );
+};
 
 export default App;
--- src/Expand.jsx
+++ src/Expand.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SlArrowUp, SlArrowDown } from 'react-icons/sl';
 
-const Expand = ({ icon, isOpen, children, title, onToggle }) => {
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button onClick={onToggle} className="expand__toggle-btn">
-          {icon}
-        </button>
+class Expand extends React.PureComponent {
+  state = {
+    isOpen: false
+  };
+  btnExtend = () => {
+    this.setState(() => ({
+      isOpen: this.state.isOpen === false ? true : false
+    }));
+  };
+  render() {
+    const { isOpen } = this.state;
+    const icon = isOpen ? <SlArrowUp /> : <SlArrowDown />;
+
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button onClick={this.btnExtend} className="expand__toggle-btn">
+            {icon}
+          </button>
+        </div>
+        {isOpen && <div className="expand__content">{this.props.children}</div>}
       </div>
-      {isOpen && <div className="expand__content">{children}</div>}
-    </div>
-  );
-};
+    );
+  }
+}
 
 Expand.protoType = {
   isOpen: PropTypes.bool,
