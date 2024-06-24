b87b2bd2193d46f2eac94b8e909eac5c5e1780bb
Denis Chornyi <chornyi.den@gmail.com>
2024-06-24 15:32:24 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// import './index.scss';
+import './index.scss';
+import Counter from './Counter.jsx';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Counter />, rootElement);
